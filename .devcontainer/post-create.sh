aba09e1d0c38f5228941c03e34989b779b73613e
fongajorge@gmail.com <0249352@up.edu.mx>
2026-02-14 19:35:28 -0600

    Initial commit

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This line ensures that the script will exit immediately if any command fails.
+set -e
+
+# Fix Volume Permissions
+sudo chown -R $(whoami): /commandhistory
+
+git clone -b 2026 --single-branch https://github.com/gcastillo56/com139-class.git 2026-2
